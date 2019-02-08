e1d197df828f1c4ca9424fc534201da3b2475d72
Jen Chow <jennego@gmail.com>
2019-02-07 22:05:12 -0800

    Minor CSS tweaks. Turns out iliterating over unnamed nested object is harder than it seems...

--- src/conponents/PhraseDisplay.js
+++ src/conponents/PhraseDisplay.js
@@ -8,52 +8,74 @@ class PhraseDisplay extends PureComponent {
 
     this.state = {
       hasError: false,
-      findWord: ' '
+      findWord: ' ',
+      word_def: 'hi'
     };
-  }
 
+  }
+  
   handleClickWord = (event) => {
     let word = event.target.innerText
     console.log(word)
     this.setState({findWord: word})
     Words
-      .getWord(word.trim())
-      .then(data => { this.setState({ word_def: data}); })
+    .getWord(word.trim())
+    .then(data => { this.setState({ word_def: data}); })
   }
   
   componentWillMount = () => {
     console.log('PhraseDisplay will mount');
   }
-
+  
   componentDidMount = () => {
     console.log('PhraseDisplay mounted');
   }
-
+  
   componentWillReceiveProps = (nextProps) => {
     console.log('PhraseDisplay will receive props', nextProps);
   }
-
+  
   componentWillUpdate = (nextProps, nextState) => {
     console.log('PhraseDisplay will update', nextProps, nextState);
   }
-
+  
   componentDidUpdate = () => {
     console.log('PhraseDisplay did update');
   }
-
+  
   componentWillUnmount = () => {
     console.log('PhraseDisplay will unmount');
   }
-
+  
   render () {
     if (this.state.hasError) {
       return <h1>Something went wrong.</h1>;
     }
+    
+    const wordList = this.state.word_def
+    
     return (
       <div className="PhraseDisplayWrapper">
-        <h1> {this.props.phrase.split(" ").map((word, k) =>
+        {/* {wordList ? 
+          Object.keys(wordList).map(type => {
+            console.log('Type: ' + type)
+            Object.keys(wordList[type]).map(syn => {
+              console.log('Word list: ' + syn)
+              wordList[type][syn].map(words => {
+                console.log(words)
+              })
+            })
+          })
+          : 'Nothing here'}  */}
+          {Object.keys(wordList).map((word) =>
+            <span> {word} </span>
+          )}
+          
+        <h1 className="main-phrase"> {this.props.phrase.split(" ").map((word, k) =>
           <span className="wordClick" onClick={this.handleClickWord}> {word} </span>
-        )}
+          )}
+
+
         
       {this.props.name} </h1> 
       </div>
